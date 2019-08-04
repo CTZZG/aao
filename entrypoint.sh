4f9ccffe6c7ae30d13fab2a70db04221b27ab629
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2019-08-04 14:47:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $PATH
+find / -name 'php'
+
 php-fpm --daemonize --fpm-config /etc/php/7.3/fpm/pool.d/www.conf
 sed -i "s/CLIENT_ID/$CLIENT_ID/g" /v2ray/config.json
 /v2ray/v2ray &
