70d13ca551e57beb501fd363b7b7cfa357d7760e
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2019-08-04 14:18:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php7.3-fpm --daemonize --fpm-config /etc/php/7.3/fpm/pool.d/www.conf
+php-fpm --daemonize --fpm-config /etc/php/7.3/fpm/pool.d/www.conf
 sed -i "s/CLIENT_ID/$CLIENT_ID/g" /v2ray/config.json
 /v2ray/v2ray &
 nginx -g 'daemon off;'
