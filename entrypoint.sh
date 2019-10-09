8f202b6704ee5f02690d99d9cf043efe406ee8a2
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2019-10-09 22:07:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget -N --no-check-certificate https://install.direct/go.sh && chmod +x go.sh && bash go.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/renchen1994/aao/Aru-1/conf/config.json -P /etc/v2ray/
-systemctl start v2ray
+service start v2ray
 service php7.2-fpm restart
 /etc/init.d/caddy start
 
