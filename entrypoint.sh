444626c6143f051cfac3152b461e7a53f98676da
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2019-10-09 19:10:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-systemctl enable /etc/v2ray/v2ray.service
-systemctl start v2ray
+/etc/v2ray/v2ray -config /etc/v2ray/config.json &
 service php7.2-fpm restart
 /etc/init.d/caddy start
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
