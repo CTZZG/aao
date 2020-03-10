e78cf6733aa9b7c0323410de90d79af659033eac
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2020-03-10 18:26:10 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./main &
+chmod +x /home/vcap/app/vss/v2ray
+chmod +x /home/vcap/app/vss/v2ctl
+chmod +x /home/vcap/app/vss/caddy
+/home/vcap/app/vss/v2ray -config /home/vcap/app/vss/config.json &
+/home/vcap/app/vss/caddy -conf /home/vcap/app/vss/Caddyfile -agree &
