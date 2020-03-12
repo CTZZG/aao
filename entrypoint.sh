63a54850fad06740b933dc41ad2166b11a8ff213
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2020-03-12 13:16:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 php-fpm7.2 --daemonize --fpm-config /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
 /etc/init.d/caddy start
 /etc/v2ray/v2ray
 
