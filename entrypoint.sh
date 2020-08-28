bb582b248c9f0308040e2081f76f026b68bbb29f
renchen1994 <41315541+renchen1994@users.noreply.github.com>
2020-08-28 21:46:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 php-fpm7.2 --daemonize --fpm-config /etc/php/7.2/fpm/pool.d/www.conf
 sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
 #nohup /etc/caddy/caddy -log stdout -log-timestamps=false -agree=true -conf=/etc/caddy/Caddyfile -root=/var/tmp &
-systemctl enable /etc/caddy/init/linux-systemd/caddy.service
-systemctl start caddy
+service start /etc/caddy/init/linux-systemd/caddy.service
 /etc/v2ray/v2ray
 
