f824b3a265a7eff0c39e97db54d486060577e89e
CTZZG <41315541+CTZZG@users.noreply.github.com>
2022-03-07 21:09:27 +0800

    Update sg1.js

--- sg1.js
+++ sg1.js
@@ -1,16 +1,10 @@
 addEventListener(
-  'fetch',event => {
+  "fetch",event => {
      let url=new URL(event.request.url);
-     url.hostname='hsg.my2g.cf';
-     if(url.protocol == 'https:') {
-        url.protocol='http:'
-     }
+     url.hostname="sg1.31vpn.com";
      let request=new Request(url,event.request);
-     if(request.headers.has("Origin")) {
-       request.headers.delete("Origin");
-     }
-     event.respondWith(
-          fetch(request)
-    )
+     event. respondWith(
+       fetch(request)
+     )
   }
 )
