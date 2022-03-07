9e69660b94b5c23ef8c5cefed1ea4821f6bd2ec7
CTZZG <41315541+CTZZG@users.noreply.github.com>
2022-03-07 21:05:23 +0800

    Update sg1.js

--- sg1.js
+++ sg1.js
@@ -1,10 +1,16 @@
 addEventListener(
-  "fetch",event => {
+  'fetch',event => {
      let url=new URL(event.request.url);
-     url.hostname="hsg.my2g.cf";
+     url.hostname='hsg.my2g.cf';
+     if(url.protocol == 'https:') {
+        url.protocol='http:'
+     }
      let request=new Request(url,event.request);
-     event. respondWith(
-       fetch(request)
-     )
+     if(request.headers.has("Origin")) {
+       request.headers.delete("Origin");
+     }
+     event.respondWith(
+          fetch(request)
+    )
   }
 )
